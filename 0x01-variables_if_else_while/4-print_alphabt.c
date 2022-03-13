a4df54d820fa377f56b76fa129ccf19f639bb102
Habenaliy <habtesfay13@gmail.com>
2022-03-13 08:37:00 -0700

    printing numbers with conditions

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,16 +4,18 @@
 *
 *Return: Always 0 (Success)
 */
-int main (void)
+int main(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		if (letter!= 'e' && letter != 'q')
+		if (letter != 'e' && letter != 'q')
 			putchar(letter);
 	}
+
 	putchar('\n');
 	
-	return(0);
+	return (0);
 }
 
