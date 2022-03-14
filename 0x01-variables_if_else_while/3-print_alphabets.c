edfc6490f59d49637207fe991a3827715028e76c
Habenaliy <99336920+Habenaliy@users.noreply.github.com>
2022-03-14 17:51:16 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,21 +4,20 @@
  * main - print alphabet in lower and uppercase
  *
  *
- * Return: (0) Succes
-**/
-
+ * Return: (0) Success
+ */
 int main(void)
 {
-  char ch;
-  
-  for (ch = 'a' ; ch <= 'z' ; ch++)
-  {
-    putchar(ch);
-  }
-  for (ch = 'A' ; ch <= 'Z' ; ch++)
-  {
-    putchar(ch);
-  }
-  putchar('\n');
-  return (0);
+char ch;
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
 }
